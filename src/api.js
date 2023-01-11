d2009be3cd7a14c1b39e4c1b17c9f82d22d2234f
Yuchan <yu.nakasone@gmail.com>
2023-01-11 14:13:45 -0300

    asd

--- src/api.js
+++ src/api.js
@@ -9,3 +9,10 @@ export const geoApiOptions = {
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+
+
+// Weather API
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "3cae3707e59eeacbd7cecb5b4f2c79df";
